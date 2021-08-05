37fbf45c30cdb9993a7de01be388d2e0266130c4
Brendon Ward <brendon.c.ward@gmail.com>
2021-08-05 20:09:46 +1000

    Blank website setup

---
+++ src/components/Landing/landingpage.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './landingpage.css';
+
+class Landing extends Component {
+    render() {
+        return(
+            <div className="landing">
+                Test
+            </div>
+        )
+    }
+}
+
+export default Landing;
